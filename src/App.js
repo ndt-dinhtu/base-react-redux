d92e9a5d37e57dcf3b6e4571d82638e81427b58b
ndt-dinhtu <ndt.290303060703>
2025-03-05 02:03:47 +0700

    khoi_tao

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+
+const App = () => {
+
+  return (
+    <div className="app-container">
+      <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+  );
+}
+
+export default App;
